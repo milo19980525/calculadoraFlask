ec928f602d870295bed9758c8a5d3216be9679f9
Camilo Enrique Rocha Calderón <106636533+milo19980525@users.noreply.github.com>
2025-06-05 01:29:22 -0500

    Update multiplicacion.py

--- multiplicacion/multiplicacion.py
+++ multiplicacion/multiplicacion.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/<num1>/<num2>")
 
 def default(num1,num2):
-    return str(int(num1)*int(num2))
+    return str(int(num1)/int(num2))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
